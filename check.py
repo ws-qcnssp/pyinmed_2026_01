a7b6082bf9fa07271a2be1b239298d5485feb9f3
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-10-24 21:25:55 +0200

    init

---
+++ check.py
@@ -0,0 +1,18 @@
+import requests
+import matplotlib
+import numpy as np
+import pandas as pd
+import pydicom
+import selenium
+import flask
+
+import os
+import sys
+
+if sys.version_info[:3] == (3,11,9) and os.path.join(os.getcwd(), '.venv').lower() in sys.executable.lower():
+    print('Wszystko ustawione!')
+else:
+    print(f'Używasz wersji pythona {sys.version_info[:3]}, z lokalizacji {sys.executable}. Sprawdź to :)')
+
+
+
