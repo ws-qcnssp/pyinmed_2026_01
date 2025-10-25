9d3dd4a2181f2b3ab679f082182302b0795a87da
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-10-25 10:58:25 +0200

    2025-10-25 10:58:25

--- glucose_validation.py
+++ glucose_validation.py
@@ -1,3 +1,8 @@
-"""
+'''
+Skrypt do walidacji stężenia glukozy we krwi
+1. wprowadzamy wartość liczbową
+2. weryfikujemy, w jakim zakresie została podana wartość
+3. weryfikujemy, czy poziom stężenia gl. jest prawidłowy
+'''
 
-"""
+
