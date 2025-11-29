846b69631333cf76d0f0766072e1bec82b1e8c87
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-11-29 15:47:44 +0100

    2025-11-29 15:47:44

--- dlugosc_zycia.py
+++ dlugosc_zycia.py
@@ -14,5 +14,6 @@ dff_k = dff.query('Dim1 == "Female"')
 
 plt.scatter(dff_m['Period'], dff_m['FactValueNumeric'], color='blue', label='mężczyźni')
 plt.scatter(dff_k['Period'], dff_k['FactValueNumeric'], color='red', label='kobiety')
+plt.legend()
 
 plt.show()
