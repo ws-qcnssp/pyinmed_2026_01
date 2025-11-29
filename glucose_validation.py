6f70e2285c961e66a9a987e8573a126ba64e03d7
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-11-29 14:09:45 +0100

    2025-11-29 14:09:45

--- glucose_validation.py
+++ glucose_validation.py
@@ -4,36 +4,37 @@ Skrypt do walidacji stężenia glukozy we krwi
 2. weryfikujemy, w jakim zakresie została podana wartość
 3. weryfikujemy, czy poziom stężenia gl. jest prawidłowy
 '''
+granica = 40.0
 
-wprowadzona_liczba = input('Wprowadź stężenie glukozy: ')
-try:
-    wprowadzona_liczba = float(wprowadzona_liczba)
-except ValueError as e:
-    print('Podana wartość nie jest liczbą, podaj wartość ułamkową z kropką, nie przecinkiem!')
-    exit()
+def wprowadzenie_zwartosci():
+    wprowadzona_liczba = input('Wprowadź stężenie glukozy: ')
+    try:
+        wprowadzona_liczba = float(wprowadzona_liczba)
+    except ValueError as e:
+        print('Podana wartość nie jest liczbą, podaj wartość ułamkową z kropką, nie przecinkiem!')
+        exit()
 # wprowadzona_liczba = float(input('Wprowadź stężenie glukozy: ')) --> jednolinijkowo
 
 # mg/dl -> 70-100 
 # mmol/l -> 3.9 - 5.6
 # wartość przyjmijmy jako 40 
 
-granica = 40.0
+def analiza_wyniku(wprowadzona_liczba):
+    if wprowadzona_liczba < granica:
+        jednostka = 'mmol/l'
+        dolna_granica = 3.9
+        gorna_granica = 5.6
+    else:
+        jednostka = 'mg/dl'
+        dolna_granica = 70
+        gorna_granica = 100
 
-if wprowadzona_liczba < granica:
-    jednostka = 'mmol/l'
-    dolna_granica = 3.9
-    gorna_granica = 5.6
-else:
-    jednostka = 'mg/dl'
-    dolna_granica = 70
-    gorna_granica = 100
-
-if dolna_granica <= wprowadzona_liczba <= gorna_granica:
-    print(f'{wprowadzona_liczba} {jednostka} -> w normie!')
-    # print('{licz} {jedn} -> w normie!'.format(licz=wprowadzona_liczba, jedn=jednostka))
-    # print(wprowadzona_liczba, jednostka, '-> w normie!')
-elif wprowadzona_liczba < dolna_granica:
-    print(f'{wprowadzona_liczba} {jednostka} -> poniżej normy!')
-else:
-    print(f'{wprowadzona_liczba} {jednostka} -> powyżej normy!')
+    if dolna_granica <= wprowadzona_liczba <= gorna_granica:
+        print(f'{wprowadzona_liczba} {jednostka} -> w normie!')
+        # print('{licz} {jedn} -> w normie!'.format(licz=wprowadzona_liczba, jedn=jednostka))
+        # print(wprowadzona_liczba, jednostka, '-> w normie!')
+    elif wprowadzona_liczba < dolna_granica:
+        print(f'{wprowadzona_liczba} {jednostka} -> poniżej normy!')
+    else:
+        print(f'{wprowadzona_liczba} {jednostka} -> powyżej normy!')
 
