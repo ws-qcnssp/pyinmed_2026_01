05f0b5ffc40e6e18f01e5ec6106338a4bb7096fe
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-11-29 15:36:56 +0100

    2025-11-29 15:36:56

--- dlugosc_zycia.py
+++ dlugosc_zycia.py
@@ -4,3 +4,7 @@ df = pd.read_csv('data.csv')
 
 # kolumna Dim1 -> płeć
 dff = df[['Indicator', 'Location', 'Dim1', 'Period', 'FactValueNumeric']]
+
+# maska = dff['Indicator'] == 'Life expectancy at birth (years)'
+# dff[dff['Indicator'] == 'Life expectancy at birth (years)']
+dff = dff.query('Indicator == "Life expectancy at birth (years)" & Location == "Poland"')
