e3813459306bc7c5c22730d63a019a8b39c5cb7d
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-11-30 13:56:20 +0100

    2025-11-30 13:56:20

--- web_automation.py
+++ web_automation.py
@@ -3,4 +3,12 @@ from playwright.sync_api import sync_playwright
 URL = 'https://the-internet.herokuapp.com'
 
 def start_pw(pw):
-    
+    browser = pw.chromium.launch(
+        headless=False,
+        slow_mo=2000
+    )
+    return browser
+
+def stop_pw(browser):
+    if browser:
+        browser.close()
