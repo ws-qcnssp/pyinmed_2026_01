d2dd848b35776d71a663570c67fe4bd95040b40f
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-11-30 14:54:10 +0100

    2025-11-30 14:54:10

--- web_automation.py
+++ web_automation.py
@@ -33,6 +33,10 @@ def test_wybor(page):
     page.goto(URL)
     page.locator("a[href*=dropdown]").click()
     page.locator("select[id=dropdown]").select_option("Option 2")
+    if page.locator("select[id=dropdown]").input_value() == '2':
+        print('Wybrano prawidłową opcję')
+    else:
+        print('oczekiwana opcja nie jest wybrana')
 
 def main():
     with sync_playwright() as pw:
