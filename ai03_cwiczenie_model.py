81198339065e29269d1e3916f2261e10983377f4
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-01-24 13:38:57 +0100

    2026-01-24 13:38:57

---
+++ ai03_cwiczenie_model.py
@@ -0,0 +1,5 @@
+# 1. Wczytaj zbiór danych przez "load_breast_cancer" z biblioteki scikit-learn
+# 2. podziel zbiór na treningowy i testowy 75% - 25%, random_state=100
+# 3. Wytrenuj model KNN (KNeighborsClassifier) z n_neighbors=5 (poszukaj w dokumentacji scikit-learn / Google)
+# 4. Oceń dokładność modelu (accuracy)
+# ------------------------------------------------------
