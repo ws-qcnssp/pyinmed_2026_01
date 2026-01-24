5bf5741eb13bddaf07d8045c179c813e6a1d957c
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-01-24 12:02:07 +0100

    2026-01-24 12:02:07

--- ai02_irysy.py
+++ ai02_irysy.py
@@ -12,6 +12,8 @@ wyniki = dane_iris.target
 print(f'Kształt cechy: {cechy.shape}')
 print(f'Kształt wyniki: {wyniki.shape}')
 
+print(wyniki)
+
 # 3. Podział danych na zbiory treningowy i testowy - 70% danych -> trening, 30% -> test
-from 
+# from 
 
