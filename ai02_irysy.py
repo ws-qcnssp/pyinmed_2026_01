5ef6844fa15d57441aeeee53d0e03b44dec67adb
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-01-24 11:12:08 +0100

    2026-01-24 11:12:08

--- ai02_irysy.py
+++ ai02_irysy.py
@@ -1,3 +1,7 @@
 # 1. wczytanie danych - gotowe dane z biblioteki scikit-learn - load_iris
 from sklearn.datasets import load_iris
 dane_iris = load_iris()
+
+# 2. Analiza danych
+print(f'Cechy w zbiorze: {dane_iris.feature_names}')
+print(f'Cechy w zbiorze: {dane_iris.target_names}')
