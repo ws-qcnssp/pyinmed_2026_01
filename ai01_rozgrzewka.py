d12318ff1e68f8e8c8c2f01177b787a954badfb2
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-01-24 09:44:06 +0100

    2026-01-24 09:44:06

--- ai01_rozgrzewka.py
+++ ai01_rozgrzewka.py
@@ -1,1 +1,20 @@
 # 1. Zaimportuj bibliotekę Pandas jako pd
+
+
+# 2. Utwórz słownik danych z trzema kluczami: 'Imię', 'Wiek', 'Miasto' i odpowiednimi listami wartości - po 5 elementów każda
+
+
+# 3. Utwórz DataFrame z powyższego słownika
+
+
+# 4. Wyświetl informacje o DataFrame za pomocą metody "info", "describe" oraz pierwsze 3 wiersze za pomocą metody "head"
+
+
+# 5. Dodaj nową kolumnę 'Kraj' z wartością 'Polska' dla wszystkich wierszy
+
+
+# 6. Filtruj DataFrame, aby wyświetlić tylko osoby starsze niż 30 lat
+
+
+# 7. Zapisz DataFrame do pliku CSV o nazwie 'dane_osobowe.csv' bez indeksów
+
