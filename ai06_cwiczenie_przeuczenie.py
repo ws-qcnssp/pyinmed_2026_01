276e0152bde108f5bfbcdb22998c43d7aa7664f9
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-02-21 13:31:29 +0100

    2026-02-21 13:31:29

--- ai06_cwiczenie_przeuczenie.py
+++ ai06_cwiczenie_przeuczenie.py
@@ -17,22 +17,11 @@ cechy_trening, cechy_test, wyniki_trening, wyniki_test = train_test_split(
     cechy, wyniki, test_size=0.4, random_state=100
 )
 
-model = KNeighborsClassifier(n_neighbors=5)
+
+model = DecisionTreeClassifier(max_depth=???)
 model.fit(cechy_trening, wyniki_trening)
 
-wyniki_przewidywane = model.predict(cechy_test)
-
-dokladnosc = accuracy_score(wyniki_test, wyniki_przewidywane)
-print(f'Dokładność (accuracy): {dokladnosc}')
-
-from sklearn.metrics import confusion_matrix, classification_report
-# 1. Wygeneruj macierz pomyłek i wypisz ją
-# 2. Wygeneruj raport klasyfikacyjny i wypisz
-macierz_konf = confusion_matrix(wyniki_test, wyniki_przewidywane)
-raport = classification_report(wyniki_test, wyniki_przewidywane, target_names=dane.target_names)
-print('Macierz pomyłek:')
-print(macierz_konf)
-print('Raport klasyfikacji:')
-print(raport)
+trening_dokladnosc = model.score(cechy_trening, wyniki_trening)
+test_dokladnosc = model.score(cechy_test, wyniki_test)
 
 
