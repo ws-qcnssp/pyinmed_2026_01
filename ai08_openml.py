495a1bf4bdc6ff4fa640e2ab2e1d0b2b15f66b01
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-02-21 14:29:46 +0100

    2026-02-21 14:29:46

--- ai08_openml.py
+++ ai08_openml.py
@@ -7,4 +7,5 @@ wyniki = dane.target
 print(cechy.head())
 print(cechy.info())
 
-# cechy_num = cechy.select_dtypes(include=['int64', ''])
+cechy_num = cechy.select_dtypes(include=['int64', 'float64']).columns
+print(cechy_num)
