d0d730a5fcbce5dff730de700c22eaf5b60f7369
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-02-21 14:44:32 +0100

    2026-02-21 14:44:32

--- ai08_openml.py
+++ ai08_openml.py
@@ -4,6 +4,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from  sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 dane = fetch_openml(data_id=1506, as_frame=True, parser='auto')
 cechy = dane.data
@@ -45,3 +46,11 @@ model_pipeline = Pipeline(
     ]
 )
 
+cechy_trening, cechy_test, wyniki_trening, wyniki_test = train_test_split(
+    cechy, wyniki, test_size=0.2, random_state=100
+)
+
+model_pipeline.fit(cechy_trening, wyniki_trening)
+wyniki_pred = model_pipeline.predict(cechy_test)
+from sklearn.metrics import classification_report
+print(classification_report(wyniki_test, wyniki_pred))
