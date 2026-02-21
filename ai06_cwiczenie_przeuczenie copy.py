a60f560c68c1a7a49fab0b0eda40cbe251a93d4f
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-02-21 14:07:29 +0100

    2026-02-21 14:07:28

---
+++ ai06_cwiczenie_przeuczenie copy.py
@@ -0,0 +1,47 @@
+# 1. Wczytaj zbiór danych przez "load_breast_cancer" z biblioteki scikit-learn
+# 2. podziel zbiór na treningowy i testowy 60% - 40%, random_state=100
+# 3. Wytrenuj model DecisionTreeClassifier -> wytrenuj z różnymi wartościami max_depth od 1 do 10
+# 4. Określ optymalną głębokość drzewa w oparciu o score dla zbiorów treningowego i testowego
+# ------------------------------------------------------
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+dane = datasets.load_breast_cancer()
+cechy = dane.data
+wyniki = dane.target
+
+# Podział na trening/test
+cechy_trening, cechy_test, wyniki_trening, wyniki_test = train_test_split(
+    cechy, wyniki, test_size=0.4, random_state=100
+)
+
+depths = []
+trening_dokladnosci = []
+test_dokladnosci = []
+
+for depth in range(1,11):
+    model = DecisionTreeClassifier(max_depth=depth, random_state=100)
+    model.fit(cechy_trening, wyniki_trening)
+
+    # dla każdego max_depth zapamiętaj depth i poniższe parametry
+    trening_dokladnosc = model.score(cechy_trening, wyniki_trening)
+    test_dokladnosc = model.score(cechy_test, wyniki_test)
+
+    depths.append(depth)
+    trening_dokladnosci.append(trening_dokladnosc)
+    test_dokladnosci.append(test_dokladnosc)
+
+    print(f'{depth}: trening - {trening_dokladnosc:.4f} ; test - {test_dokladnosc:.4f}')
+
+# wypisz wyniki / narysuj wykres
+
+import matplotlib.pyplot as plt
+plt.plot(depths, trening_dokladnosci, label = 'trening - dokladnosc')
+plt.plot(depths, test_dokladnosci, label = 'test - dokladnosc')
+plt.legend()
+plt.show()
+
+
+
