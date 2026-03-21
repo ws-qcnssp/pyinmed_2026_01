894dfee46ab38a3b7ad3463265b6753a8e4929d6
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 09:40:46 +0100

    2026-03-21 09:40:46

---
+++ ai09_klasyfikacja_MLP.py
@@ -0,0 +1,7 @@
+# 1. Wczytaj zbiór danych przez "load_breast_cancer" z biblioteki scikit-learn
+# 2. podziel zbiór na treningowy i testowy 75% - 25%, random_state=100
+# 3. Wytrenuj model MLP (MLPClassifier), z jedną warstwą ukrytą o wielkości 50 jednostek
+#  (poszukaj w dokumentacji scikit-learn / Google)
+# 4. Oceń dokładność modelu (accuracy)
+# ------------------------------------------------------
+
