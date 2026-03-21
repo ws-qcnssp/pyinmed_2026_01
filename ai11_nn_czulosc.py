68f27935867c0ff2349d9f75495f1acedc8def4b
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 11:03:31 +0100

    2026-03-21 11:03:31

--- ai11_nn_czulosc.py
+++ ai11_nn_czulosc.py
@@ -4,8 +4,10 @@ dane = fetch_openml(data_id=1466, as_frame=True, parser='auto')
 cechy = dane.data # X
 wyniki = dane.target # y
 
+print('Cechy:')
 print(cechy.describe())
 print(cechy.head())
 
+print('Wyniki:')
 print(wyniki.head())
 
