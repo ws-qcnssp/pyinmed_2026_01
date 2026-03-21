330ae89fa84cba8f488c6365fd015625749262ba
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 11:55:36 +0100

    2026-03-21 11:55:36

--- ai12_nn_optymalizacja.py
+++ ai12_nn_optymalizacja.py
@@ -3,6 +3,7 @@ from sklearn.neural_network import MLPClassifier
 # from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.metrics import accuracy_score
+import pandas as pd
 
 # Wczytanie danych z OpenML - badania ILPD
 dane = fetch_openml(data_id=1480, as_frame=True, parser='auto')
@@ -15,3 +16,6 @@ print(cechy.head())
 
 print('Wyniki:')
 print(wyniki.head())
+
+cechy = pd.get_dummies(cechy, columns=['V2'], drop_first=True)
+print(cechy.head())
