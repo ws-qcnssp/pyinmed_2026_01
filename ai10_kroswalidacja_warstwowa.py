363b823e448f2868fdc023edf1d317629c6aee8a
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 10:12:39 +0100

    2026-03-21 10:12:38

---
+++ ai10_kroswalidacja_warstwowa.py
@@ -0,0 +1,9 @@
+
+
+dane = {
+    'Wiek': [45, 62, 33, 70, 55, 35, 60, 39, 75, 29],
+    'CRP': [5.1, 12.5, 18, 15.0, 7.2, 4.5, 11.1, 2.0, 18.3, 1.5],
+    'Plec': ['K', 'M', 'K', 'M', 'K', 'M', 'K', 'K', 'M', 'M'],
+    'Palenie': ['Tak', 'Nie', 'Nie', 'Tak', 'Nie', 'Tak', 'Nie', 'Nie', 'Tak', 'Nie'],
+    'Wynik': [1, 1, 0, 1, 0, 1, 1, 0, 1, 0] # 1 = Podwyższone ryzyko, 0 = Niskie ryzyko
+}
