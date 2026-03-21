d1d6ab0d738cffff687064911caf247a6540548c
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 10:02:10 +0100

    2026-03-21 10:02:10

--- ai09_klasyfikacja_MLP.py
+++ ai09_klasyfikacja_MLP.py
@@ -5,3 +5,26 @@
 # 4. Oceń dokładność modelu (accuracy)
 # ------------------------------------------------------
 
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+# from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+dane = datasets.load_breast_cancer()
+cechy = dane.data
+wyniki = dane.target
+
+# Podział na trening/test
+cechy_trening, cechy_test, wyniki_trening, wyniki_test = train_test_split(
+    cechy, wyniki, test_size=0.25, random_state=100
+)
+
+# model = KNeighborsClassifier(n_neighbors=5)
+model = MLPClassifier(hidden_layer_sizes=(50))
+model.fit(cechy_trening, wyniki_trening)
+
+wyniki_przewidywane = model.predict(cechy_test)
+
+dokladnosc = accuracy_score(wyniki_test, wyniki_przewidywane)
+print(f'Dokładność (accuracy): {dokladnosc}')
