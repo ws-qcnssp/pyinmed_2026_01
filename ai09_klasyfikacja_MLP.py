237c55c57667a0b10b70f3f57893dbde94e9b1d6
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 10:02:20 +0100

    2026-03-21 10:02:20

--- ai09_klasyfikacja_MLP.py
+++ ai09_klasyfikacja_MLP.py
@@ -7,7 +7,6 @@
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-# from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
@@ -20,7 +19,6 @@ cechy_trening, cechy_test, wyniki_trening, wyniki_test = train_test_split(
     cechy, wyniki, test_size=0.25, random_state=100
 )
 
-# model = KNeighborsClassifier(n_neighbors=5)
 model = MLPClassifier(hidden_layer_sizes=(50))
 model.fit(cechy_trening, wyniki_trening)
 
