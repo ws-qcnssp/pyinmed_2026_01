893a59b1ba32214ea9d0e0edbcaad56a2f49cc60
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2026-03-21 11:50:42 +0100

    2026-03-21 11:50:42

---
+++ ai12_nn_optymalizacja.py
@@ -0,0 +1,5 @@
+from sklearn.datasets import fetch_openml
+from sklearn.neural_network import MLPClassifier
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.metrics import accuracy_score
